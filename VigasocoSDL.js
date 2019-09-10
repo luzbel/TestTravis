dcba3ac81594de13bdab4cfe61ce0b56a782290d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-10 16:25:54 +0000

    Deploy luzbel/TestTravis to github.com/luzbel/TestTravis.git:gh-pages

--- VigasocoSDL.js
+++ VigasocoSDL.js
@@ -195,7 +195,7 @@ Module['FS_createPath']('/roms', 'abadia', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 1, "end": 0, "filename": "/roms/abadia/final.wav"}, {"start": 0, "audio": 0, "end": 174065, "filename": "/roms/abadia/GraficosVGA"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/cerrar.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/campanas.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/fondo.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/inicio.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/aporrear.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/coger.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/dejar.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/abrir.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/tintineo.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/pasos.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/espejo.wav"}, {"start": 174065, "audio": 0, "end": 334833, "filename": "/roms/abadia/abadia.dsk"}, {"start": 334833, "audio": 0, "end": 508898, "filename": "/roms/abadia/GraficosCPC"}], "remote_package_size": 508898, "package_uuid": "cdeccfbf-33cc-4ed6-8463-eb94f1613061"});
+ loadPackage({"files": [{"start": 0, "audio": 1, "end": 0, "filename": "/roms/abadia/final.wav"}, {"start": 0, "audio": 0, "end": 174065, "filename": "/roms/abadia/GraficosVGA"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/cerrar.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/campanas.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/fondo.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/inicio.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/aporrear.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/coger.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/dejar.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/abrir.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/tintineo.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/pasos.wav"}, {"start": 174065, "audio": 1, "end": 174065, "filename": "/roms/abadia/espejo.wav"}, {"start": 174065, "audio": 0, "end": 334833, "filename": "/roms/abadia/abadia.dsk"}, {"start": 334833, "audio": 0, "end": 508898, "filename": "/roms/abadia/GraficosCPC"}], "remote_package_size": 508898, "package_uuid": "86928e0d-8f15-4c75-93ce-fed7fedba46d"});
 
 })();
 
@@ -1408,11 +1408,11 @@ function updateGlobalBufferAndViews(buf) {
 if (!ENVIRONMENT_IS_PTHREAD) { // Pthreads have already initialized these variables in src/worker.js, where they were passed to the thread worker at startup time
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 144048,
+    STACK_BASE = 144032,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 5386928,
-    DYNAMIC_BASE = 5386928,
-    DYNAMICTOP_PTR = 144016;
+    STACK_MAX = 5386912,
+    DYNAMIC_BASE = 5386912,
+    DYNAMICTOP_PTR = 144000;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -1963,7 +1963,7 @@ function _initPthreadsJS(){ PThread.initRuntime(); }
 
 
 
-// STATICTOP = STATIC_BASE + 143024;
+// STATICTOP = STATIC_BASE + 143008;
 /* global initializers */ if (!ENVIRONMENT_IS_PTHREAD) __ATINIT__.push({ func: function() { globalCtors() } });
 
 
@@ -1977,7 +1977,7 @@ memoryInitializer = "VigasocoSDL.html.mem";
 
 /* no memory initializer */
 var tempDoublePtr;
-if (!ENVIRONMENT_IS_PTHREAD) tempDoublePtr = 144032;
+if (!ENVIRONMENT_IS_PTHREAD) tempDoublePtr = 144016;
 assert(tempDoublePtr % 8 == 0);
 
 function copyTempFloat(ptr) { // functions, because inlining this code increases code size too much
@@ -7373,7 +7373,7 @@ function copyTempDouble(ptr) {
   var __pthread_is_main_browser_thread=0; 
   
   
-  var __main_thread_futex_wait_address; if (ENVIRONMENT_IS_PTHREAD) __main_thread_futex_wait_address = PthreadWorkerInit.__main_thread_futex_wait_address; else PthreadWorkerInit.__main_thread_futex_wait_address = __main_thread_futex_wait_address = 144000;function _emscripten_futex_wake(addr, count) {
+  var __main_thread_futex_wait_address; if (ENVIRONMENT_IS_PTHREAD) __main_thread_futex_wait_address = PthreadWorkerInit.__main_thread_futex_wait_address; else PthreadWorkerInit.__main_thread_futex_wait_address = __main_thread_futex_wait_address = 143984;function _emscripten_futex_wake(addr, count) {
       if (addr <= 0 || addr > HEAP8.length || addr&3 != 0 || count < 0) return -22;
       if (count == 0) return 0;
       // Waking (at least) INT_MAX waiters is defined to mean wake all callers.
@@ -7406,7 +7406,7 @@ function copyTempDouble(ptr) {
         _emscripten_register_main_browser_thread_id(PThread.mainThreadBlock);
       },initMainThreadBlock:function () {
         if (ENVIRONMENT_IS_PTHREAD) return undefined;
-        PThread.mainThreadBlock = 143216;
+        PThread.mainThreadBlock = 143200;
   
         for (var i = 0; i < 244/4; ++i) HEAPU32[PThread.mainThreadBlock/4+i] = 0;
   
@@ -7419,7 +7419,7 @@ function copyTempDouble(ptr) {
         HEAP32[((headPtr)>>2)]=headPtr;
   
         // Allocate memory for thread-local storage.
-        var tlsMemory = 143472;
+        var tlsMemory = 143456;
         for (var i = 0; i < 128; ++i) HEAPU32[tlsMemory/4+i] = 0;
         Atomics.store(HEAPU32, (PThread.mainThreadBlock + 116 ) >> 2, tlsMemory); // Init thread-local-storage memory array.
         Atomics.store(HEAPU32, (PThread.mainThreadBlock + 52 ) >> 2, PThread.mainThreadBlock); // Main thread ID.
@@ -9252,7 +9252,7 @@ function copyTempDouble(ptr) {
       return _strftime(s, maxsize, format, tm); // no locale support yet
     }
 
-  var ___dso_handle; if (ENVIRONMENT_IS_PTHREAD) ___dso_handle = PthreadWorkerInit.___dso_handle; else PthreadWorkerInit.___dso_handle = ___dso_handle = 143056;
+  var ___dso_handle; if (ENVIRONMENT_IS_PTHREAD) ___dso_handle = PthreadWorkerInit.___dso_handle; else PthreadWorkerInit.___dso_handle = ___dso_handle = 143040;
 
 FS.staticInit();Module["FS_createFolder"] = FS.createFolder;Module["FS_createPath"] = FS.createPath;Module["FS_createDataFile"] = FS.createDataFile;Module["FS_createPreloadedFile"] = FS.createPreloadedFile;Module["FS_createLazyFile"] = FS.createLazyFile;Module["FS_createLink"] = FS.createLink;Module["FS_createDevice"] = FS.createDevice;Module["FS_unlink"] = FS.unlink;;
 if (ENVIRONMENT_HAS_NODE) { var fs = require("fs"); var NODEJS_PATH = require("path"); NODEFS.staticInit(); };
